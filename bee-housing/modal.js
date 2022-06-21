fa53e5d2947c843e16c900d6a6258e271efcf79d
Shaun Halliday <100038663+smolCat1512@users.noreply.github.com>
2022-06-21 13:32:51 +0100

    Add files via upload
    Site complete

--- bee-housing/modal.js
+++ bee-housing/modal.js
@@ -10,3 +10,24 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
     modal_container.classList.remove('show');
 });
+
+const handleSubmit = async () => {
+    alert(`Thanks for logging your complaint. Your feedback is very important to us and we're keen to resolve any of your concerns swiftly. We'll be in touch with you shortly`);
+    const from = document.getElementById('from').value;
+    const body = document.getElementById('body').value;
+    console.log(from, body);
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({ "from": from, "body": body });
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    fetch("https://licorice-eland-9853.twil.io/create-complaint", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    return false;
+};
